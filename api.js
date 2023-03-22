03d55d6305463cc8e39af4a934afbb4f9e31dc1f
JLoGukA <JLoGuka@gmail.com>
2023-03-22 19:50:57 +0300

    Now can generate audio from text

--- api.js
+++ api.js
@@ -1,23 +1,14 @@
-const router = require('express').Router();
-
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const http=require('http')
+const mysql = require('mysql2');
+const fs =require('fs')
+const formidable = require('formidable');
+const axios=require('axios');
+const { exec } = require("child_process");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
-
-const mysql = require('mysql2');
-const { get } = require('http');
-const fs =require('fs')
-const url = require( "url" );
-const queryString = require( "querystring" );
-const FormData = require('form-data');
-const formidable = require('formidable');
-
-
-
 
 const con = mysql.createConnection({
     host: 'localhost',
@@ -27,48 +18,145 @@ const con = mysql.createConnection({
     database:"st"
 });
 
+var onUpload=""
+
 async function checkOnline(){
     let queryIp=(await con.promise().query("select distinct id,ip from st.devices"))
     
     for(let i=0;i<queryIp[0].length;i++){
         let str=queryIp[0][i].ip
-        http.get({host:str}, function(res) {
-        if (res.statusCode == 200) {
-          console.log("success");
-        }
-        }).on('error', function(e) {
-            con.query("delete from devices where id ="+queryIp[0][i].id,(err, result, fields) => {console.log(result)});
-        });
+        await axios.get("http://"+str+":80/isAlive").then((res)=>{/*res.data should be "Alive"*/ }).catch((error)=>{
+            con.query("delete from devices where id ="+queryIp[0][i].id,(err, result, fields) => {/*console.log(result)*/});
+        })
     }
+    
 }
-
-//setInterval(checkOnline,5000)
+//setInterval(checkOnline,180000)
 app.get('/', (req, res) => {
     res.sendStatus(200)
 });
 
-app.post('/file/upload',(req, res) => {
+app.get('/file/generate',async(req, ress) => {
+    
+    let path = __dirname+"/genAudio"
+
+    if(req.headers.file!==undefined){
+        let decname = decodeURI(req.headers.file)
+        let dectext = decodeURI(req.headers.text)
+        exec("echo \""+dectext+"\" | RHVoice-test -o \"/$home/gen/"+decname+" -p aleksandr-hq",(error,stdout,stderr)=>{
+            if(error)console.log(error)
+            if(stderr)console.log(stderr)
+        })
+        
+    }
+//     Место файла конфига:
+// sudo nano /usr/local/etc/RHVoice/RHVoice.conf
+
+// Тест:
+// echo "Привет!" | RHVoice-test -o "/mnt/c/Games/wav/1.wav" -p aleksandr-hq
+
+    
+
+
+
+
+    let files=fs.readdirSync(path)
+    ress.send(files)
+})
+
+app.post('/file/upload',async(req, ress) => {
     let options={
-        maxFileSize:20* 1024 * 1024
+        maxFileSize:20* 1024 * 1024 //20MB
     }
     const form = new formidable.IncomingForm(options);
-    form.parse(req, (err, fields, files) => {
-        let raw = fs.readFileSync(files.file.filepath)
-        fs.writeFile(fields.saveDir+fields.fileName, raw,(err)=>{
+    form.parse(req, async (err, fields, files) => {
+        let raw = fs.readFileSync(files.file.filepath)  
+        
+        //fields.saveDir - directory to save on device      
+        fs.writeFile("filesToUpload/"+fields.fileName, raw,(err)=>{
+        })
+        onUpload="filesToUpload\\"+fields.fileName
+        let fileDir=fields.saveDir
+        if(fileDir[0]!=='/')fileDir='/'+fileDir
+        if(fileDir[fileDir.length-1]!=='/')fileDir+='/'
+        let config = {
+            headers: {
+                'file': String(fileDir+fields.fileName),
+                'dir':String(fileDir),
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+        await axios.post("http://"+fields.ip+":80/upload",{raw},config).then((res)=>{/*console.log(res.statusText)*/}).catch((error)=>{
+            console.log(error.cause)
+        })
+        await axios.get("http://"+fields.ip+":80/getResources").then((res)=>{
+            ress.send(res.status)
         })
     });
-    res.sendStatus(200)
-
-});
-
-
-app.get('/probe',async (req,res)=>{
-    let path=__dirname+'\\files\\win.png';
-    // fs.open(__dirname+'\\files\\win.png','r',function(err,f){
-    //     if(err)console.log(err)
-    // })
-    res.sendFile(path)
 })
+
+app.get('/file/download',async(req, ress) => {
+    const options={
+        root:__dirname
+    }
+    if(req.headers===undefined)ress.sendStatus(404);
+
+    if(req.headers.gen!==undefined){
+        let decname =decodeURI(req.headers.filename)
+        let path = "genAudio/"+decname
+        ress.sendFile(path,options,(err)=>{
+            if(err)console.log(err)
+        })
+    }
+    else{
+        let writer=fs.createWriteStream("filesToUpload/"+req.headers.filename,{highWaterMark: Math.pow(2,16)})
+        writer.on('close',()=>{
+            ress.sendFile("filesToUpload/"+req.headers.filename,options)
+        })
+        await axios({
+            url:"http://"+req.headers.ip+":80/download",
+            method:'GET',
+            responseType:'stream',
+            headers:{
+                'file': req.headers.file,
+            }
+        }).then((res)=>{
+            res.data.pipe(writer)
+            
+        }).catch((error)=>{console.log(error)})
+    }
+
+    
+})
+
+app.post('/file/delete',async(req, ress) => {
+
+    
+    if(req.headers.gen!==undefined){
+        let decname = decodeURI(req.headers.filename)
+        await fs.promises.unlink(__dirname+"/genAudio/"+decname)
+        ress.send(200)
+    }
+    else{
+        await axios({
+            url:"http://"+req.headers.ip+":80/delete",
+            method:'GET',
+            headers:{
+                'dir': req.headers.dir,
+            }
+        }).then((res)=>{
+    
+        }).catch((error)=>{console.log(error.cause)}).then(async()=>{
+            await axios.get("http://"+req.headers.ip+":80/getResources").then((res)=>{
+                ress.send(res.status)
+            })
+        })
+    }
+
+    
+    
+})
+
 
 app.post('/device/sendInfo', (req, res) => {
 
@@ -80,7 +168,7 @@ app.post('/device/sendInfo', (req, res) => {
     req.body.ID + 
     " in(select id from st.devices),1,0) as pass",(err, result, fields) => {
         
-        if(result[0].pass)con.query("delete from st.devices where id="+req.body.ID);
+        if(result!=undefined&&result[0].pass)con.query("delete from st.devices where id="+req.body.ID);
         
         for(let i=0; i<array.length-2; i++){
             con.query("insert st.devices(id,ip,File) values(" +
@@ -93,15 +181,17 @@ app.post('/device/sendInfo', (req, res) => {
                         ,(err, result, fields) => {})
         }
     }); 
+    //отправить в отдельную таблицу ИД устройства
+    
 });
 
 app.get('/device/info', async (req, res) => {
+    
     var deviceInfo=[]
     var deviceFile=[]
-    let tempArray=[]
     
-    let ress = (await con.promise().query("select distinct id,ip,count(file) as cnt from st.devices group by id,ip"))
-    let queryFile=(await con.promise().query("select file from st.devices"))
+    let ress = (await con.promise().query("select distinct id,ip,count(file) as cnt from st.devices group by id,ip").catch((err)=>{return}))
+    let queryFile=(await con.promise().query("select file from st.devices").catch((err)=>{return}))
     
     for(let i in ress[0]){
         deviceInfo.push([ress[0][i].id,ress[0][i].ip,ress[0][i].cnt])
@@ -114,8 +204,6 @@ app.get('/device/info', async (req, res) => {
         }
         k+=deviceInfo[i][2]
     }
-
-    
 
     res.send({deviceInfo,deviceFile})
 
@@ -153,7 +241,6 @@ app.get('/get/music', (req, res) => {
         }
         res.send([musicList,musicVotes,songsAmount,votesAmount]);
     });
-
 });
 
 app.get('/get/sched', (req, res) => {
@@ -174,10 +261,6 @@ app.get('/get/schedule', (req, res) => {
     });
 
 });
-
-function setValue(value){
-    tempValue=value;
-}
 
 app.get('/get/winner',async(req, res) => {
 
